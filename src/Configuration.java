8fdfd623bff3e44eec723e216997e8bd5e2c72fa
Michael O'Connor <me@seeocon.co>
2016-11-30 18:31:51 -0500

    Initial Commit

---
+++ src/Configuration.java
@@ -0,0 +1,19 @@
+
+public class Configuration {
+
+	public static final double VERSION = 1.0;
+	public static final String CLIENT_TITLE = "Foxtrot 718/753 (v"+VERSION+")";
+	
+	public static final String SERVER_IP = "127.0.0.1";
+	public static final int SERVER_PORT = 43594;
+	
+	public static final int REVISION = 718;
+	public static final int SUB_REVISION = 1;
+	
+	public static final boolean LOBBY_ENABLED = false;
+	public static final boolean DISABLE_XTEA_CRASH = true;
+	public static final boolean DISABLE_USELESS_PACKETS = true;
+	public static final boolean DISABLE_RSA = false;
+	public static final boolean useIsaac = false;
+	public static final boolean usingRS = false;
+}
